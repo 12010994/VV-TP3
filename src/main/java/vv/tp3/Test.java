5d5012a15e9c4feb49fab8e7ddbab5b2438fe848
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-10-10 14:11:04 +0200

    first commit

---
+++ src/main/java/vv/tp3/Test.java
@@ -0,0 +1,13 @@
+package vv.tp3;
+
+/**
+ * Created by vpetit on 08/10/14.
+ */
+public class Test {
+
+    public static void main(final String[] args) {
+        while(true){
+            while(true){}
+        }
+    }
+}
---
+++ src/main/java/vv/tp3/WhileLoopsMustNotInterlockRule.java
@@ -0,0 +1,33 @@
+package vv.tp3;
+
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.*;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+
+import java.util.List;
+
+public class WhileLoopsMustNotInterlockRule extends AbstractJavaRule {
+
+    public Object visit(ASTWhileStatement node, Object data) {
+
+        Node firstStmt;
+        firstStmt = (Node)node.jjtGetChild(1);
+        if (hasWhileStatement(firstStmt)) {
+            //ajout de la violation
+            addViolation(data, node);
+        }
+        return super.visit(node,data);
+    }
+
+    private boolean hasWhileStatement(Node node) {
+        for(int i=0; i< node.jjtGetNumChildren(); i++){
+            if (node.jjtGetChild(i) instanceof ASTWhileStatement) {
+                return true;
+            }
+        }
+        List<ASTWhileStatement> childrenOfType = node.findChildrenOfType(ASTWhileStatement.class);
+        if (childrenOfType != null) return true;
+
+        return false;
+    }
+}
