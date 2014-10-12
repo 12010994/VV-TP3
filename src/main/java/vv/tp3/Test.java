184b58462a4bedf8a80af49f5e5f5e1f45a5d7ad
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-10-12 14:23:50 +0200

    update

--- src/main/java/vv/tp3/Test.java
+++ src/main/java/vv/tp3/Test.java
@@ -3,26 +3,22 @@ package vv.tp3;
 public class Test {
 
     public static void main(final String[] args) {
-        //testBoucleImbriquees();
-        testWhile(true);
-        //testWhile(false);
+
     }
 
-    private static void testBoucleImbriquees() {
-        while(true){
-            while(true){
-                while(true){
-                    break;
+    private static void testBoucleImbriquees(boolean a, boolean b, boolean c) {
+        while(a){
+            while(b){
+                while(c){
                 }
-                break;
             }
-            break;
         }
     }
 
-    private static void testWhile(boolean b) {
+    private static void testWhile() {
         while(true){
-            break;
+        }
+        while(false){
         }
     }
 }
